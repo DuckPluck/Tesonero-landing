262bd0bc76aecb22f834c396da4c15aa11ff299a
Даниил Уткин <dan4ezzz@gmail.com>
2023-04-01 01:48:46 +0300

    fixing ie11 compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from "@vitejs/plugin-legacy";
+import "@babel/polyfill";
+import { defineConfig } from 'vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Tesonero-landing/',
-  plugins: [react()],
+  plugins: [
+      react(),
+      legacy({
+          // for ie11
+          targets: ["ie >= 11"],
+          additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+          polyfills: ["es.array.iterator"],
+      }),
+  ],
 })
